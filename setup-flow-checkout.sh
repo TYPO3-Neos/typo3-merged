0a8a24295ecf43af4aaeae6dab16365017cef3c5
Karsten Dambekalns <karsten@typo3.org>
2015-02-26 17:08:35 +0100

    [TASK] Remove FLOW3 1.0 and 1.1 from the configuration

--- setup-flow-checkout.sh
+++ setup-flow-checkout.sh
@@ -9,11 +9,3 @@ fi
 if [ ! -d "Flow-2.X" ]; then
     cp -Ra Flow-master Flow-2.X
 fi
-
-if [ ! -d "FLOW3-1.1" ]; then
-    git clone -b FLOW3-1.1 --recursive git://git.typo3.org/Flow/Distributions/Base.git FLOW3-1.1
-fi
-
-if [ ! -d "FLOW3-1.0" ]; then
-    git clone -b FLOW3-1.0 --recursive git://git.typo3.org/Flow/Distributions/Base.git FLOW3-1.0
-fi
