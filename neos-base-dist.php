9a09fb75a5dda3219755f143b9a365c867f18a0c
Karsten Dambekalns <karsten@typo3.org>
2015-03-13 11:04:42 +0100

    [TASK] Dropping some 1.0 changes for Neos

--- neos-base-dist.php
+++ neos-base-dist.php
@@ -31,7 +31,15 @@ $projectsToCheck = array(
 		),
 		'ignoreList' => array(
 			'1.0' => array(
-				'989e33743da6dd93076dfcac957fa2376bd84054' => 'Wrong releases footer, came from 1.0'
+				'I989e33743da6dd93076dfcac957fa2376bd84054' => 'Wrong releases footer, came from 1.0',
+				'I09ae20ae62087ae6b7d4eeb183a3b45e0b5bafbd' => 'Not fixing for 1.0 anymore',
+				'NEOS-173' => 'Not fixing for 1.0 anymore',
+				'NEOS-361' => 'Not fixing for 1.0 anymore',
+				'NEOS-368' => 'Not fixing for 1.0 anymore',
+				'NEOS-352' => 'Not fixing for 1.0 anymore',
+				'NEOS-585' => 'Not fixing for 1.0 anymore',
+				'Id7baa52bd72cf6e53d4ba8dee9cad8f54979aa40' => 'Not fixing for 1.0 anymore',
+				'I78ee51099beef563c56c242bc3b2e6ce10efd4f7' => 'Not fixing for 1.0 anymore'
 			),
 		),
 	),
@@ -42,6 +50,11 @@ $projectsToCheck = array(
 			array('1.1', 'b3c147d2c17309fde9e36a09a72f70782248f9e2', 'origin/1.1', 'Neos-1.1/Packages/Application/TYPO3.Neos.NodeTypes'),
 			array('1.2', 'b3c147d2c17309fde9e36a09a72f70782248f9e2', 'origin/1.2', 'Neos-1.2/Packages/Application/TYPO3.Neos.NodeTypes'),
 			array('master', 'b3c147d2c17309fde9e36a09a72f70782248f9e2', 'origin/master', 'Neos-master/Packages/Application/TYPO3.Neos.NodeTypes'),
+		),
+		'ignoreList' => array(
+			'1.0' => array(
+				'Ie9c134214203d3a928da601e37a4698a9f4ba5a3' => 'Not fixing for 1.0 anymore'
+			),
 		),
 	),
 	'TYPO3.SiteKickstarter' => array(
@@ -63,7 +76,12 @@ $projectsToCheck = array(
 		),
 		'ignoreList' => array(
 			'1.0' => array(
-				'I99acc1142eeaac82f443f5efa1512feb7766ed1f' => 'Wrong releases footer, does not apply to 1.0'
+				'I99acc1142eeaac82f443f5efa1512feb7766ed1f' => 'Wrong releases footer, does not apply to 1.0',
+				'NEOS-879' => 'Not fixing for 1.0 anymore',
+				'I3dc57f55ba052bee2399ba5b97e5f985fd0a4e3a' => 'Not fixing for 1.0 anymore',
+				'If4d3dff4027f1711a548b75ea78ec45682b89773' => 'Not fixing for 1.0 anymore',
+				'NEOS-170' => 'Not fixing for 1.0 anymore',
+				'I56f8e922f84c00d9402837591d308583f7069b3f' => 'Not fixing for 1.0 anymore'
 			),
 		),
 	),
@@ -75,9 +93,14 @@ $projectsToCheck = array(
 			array('1.2', '681aa987a62d555b77d8398f38cc6280cb0c5fd6', 'origin/1.2', 'Neos-1.2/Packages/Application/TYPO3.TypoScript'),
 			array('master', '681aa987a62d555b77d8398f38cc6280cb0c5fd6', 'origin/master', 'Neos-master/Packages/Application/TYPO3.TypoScript'),
 		),
+		'ignoreList' => array(
+			'1.0' => array(
+				'Idffd41450c3c210508795aaeda69ac4e199cc54e' => 'Not fixing for 1.0 anymore'
+			),
+		),
 	),
 	'TYPO3.NeosDemoTypo3Org' => array(
-		'gitWebUrl' => 'http://git.typo3.org/Packages/TYPO3.NeosDemoTypo3Org.git',
+		'gitWebUrl' => 'http://git.typo3.org/Packages/NeosDemoTypo3Org.git',
 		'releases' => array(
 			array('1.0', '4766bdd615b63bcefc8d2862738e33a2b3313448', 'origin/1.0', 'Neos-1.0/Packages/Sites/TYPO3.NeosDemoTypo3Org'),
 			array('1.1', '4766bdd615b63bcefc8d2862738e33a2b3313448', 'origin/1.1', 'Neos-1.1/Packages/Sites/TYPO3.NeosDemoTypo3Org'),
@@ -92,6 +115,12 @@ $projectsToCheck = array(
 			array('1.1', '3189fc5ec371b46c012443b0c5cb49c225c76b1e', 'origin/1.1', 'Neos-1.1/Packages/Application/TYPO3.Media'),
 			array('1.2', '3189fc5ec371b46c012443b0c5cb49c225c76b1e', 'origin/1.2', 'Neos-1.2/Packages/Application/TYPO3.Media'),
 			array('master', '70290a04f07329ccb725181c6d7e028e0aeac483', 'origin/master', 'Neos-master/Packages/Application/TYPO3.Media'),
+		),
+		'ignoreList' => array(
+			'1.0' => array(
+				'NEOS-414' => 'Not fixing for 1.0 anymore',
+				'Icbf9110b19bdd705a8db7e2d67521225651b6dad' => 'Not fixing for 1.0 anymore'
+			),
 		),
 	)
 );
