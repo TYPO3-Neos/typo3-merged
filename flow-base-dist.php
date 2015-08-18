d06d9eb1f6efc1d53677c94d7fc999adc20fd386
Karsten Dambekalns <karsten@neos.io>
2015-08-18 23:33:29 +0200

    [TASK] Removing Flow/Neos branches having reached EOL

--- flow-base-dist.php
+++ flow-base-dist.php
@@ -15,8 +15,6 @@ $projectsToCheck = array(
 		'gitWebUrl' => 'https://github.com/neos/flow-base-distribution',
 		'releases' => array(
 				# project, starting point, branch, working copy path
-			array('2.0', 'refs/tags/FLOW3-1.0.0', 'origin/2.0', 'Flow-2.X'),
-			array('2.1', 'refs/tags/2.0.0', 'origin/2.1', 'Flow-2.X'),
 			array('2.2', 'refs/tags/2.0.0', 'origin/2.2', 'Flow-2.X'),
 			array('2.3', 'refs/tags/2.0.0', 'origin/2.3', 'Flow-2.X'),
 			array('3.0', 'refs/tags/2.0.0', 'origin/3.0', 'Flow-3.X'),
@@ -34,29 +32,12 @@ $projectsToCheck = array(
 	'TYPO3.Flow' => array(
 		'gitWebUrl' => 'https://github.com/neos/flow',
 		'releases' => array(
-			array('2.0', 'refs/tags/FLOW3-1.0.0', 'origin/2.0', 'Flow-2.X/Packages/Framework/TYPO3.Flow'),
-			array('2.1', 'refs/tags/2.0.0', 'origin/2.1', 'Flow-2.X/Packages/Framework/TYPO3.Flow'),
 			array('2.2', 'refs/tags/2.0.0', 'origin/2.2', 'Flow-2.X/Packages/Framework/TYPO3.Flow'),
 			array('2.3', 'refs/tags/2.0.0', 'origin/2.3', 'Flow-2.X/Packages/Framework/TYPO3.Flow'),
 			array('3.0', 'refs/tags/2.0.0', 'origin/3.0', 'Flow-3.X/Packages/Framework/TYPO3.Flow'),
 			array('master', 'refs/tags/2.0.0', 'origin/master', 'Flow-master/Packages/Framework/TYPO3.Flow'),
 		),
 		'ignoreList' => array(
-			'2.0' => array(
-				'48231' => 'That bugfix was broken and thus abandoned',
-				'51257' => 'Change not needed for 2.0',
-				'50636' => 'Not applicable to 2.0',
-				'54453' => 'Not applicable to 2.0',
-				'42961' => 'Not a bugfix, master only',
-				'51676' => 'Change had side effects',
-				'I269a9e12aa5fbd50a90bd81c094fe2bc315b5222' => 'Not needed, base change was master only'
-			),
-			'2.1' => array(
-				'42961' => 'Not a bugfix, master only',
-				'50118' => 'Not a bugfix, master only',
-				'33621' => 'No longer needed, remaining changes in https://review.typo3.org/27840',
-				'I269a9e12aa5fbd50a90bd81c094fe2bc315b5222' => 'Not needed, base change was master only'
-			),
 			'master' => array(
 				'46053' => 'In master as for #40738',
 				'33621' => 'No longer needed, remaining changes in https://review.typo3.org/27840',
@@ -67,8 +48,6 @@ $projectsToCheck = array(
 	'TYPO3.Fluid' => array(
 		'gitWebUrl' => 'https://github.com/neos/fluid',
 		'releases' => array(
-			array('2.0', 'refs/tags/FLOW3-1.0.0', 'origin/2.0', 'Flow-2.X/Packages/Framework/TYPO3.Fluid'),
-			array('2.1', 'refs/tags/2.0.0', 'origin/2.1', 'Flow-2.X/Packages/Framework/TYPO3.Fluid'),
 			array('2.2', 'refs/tags/2.0.0', 'origin/2.2', 'Flow-2.X/Packages/Framework/TYPO3.Fluid'),
 			array('2.3', 'refs/tags/2.0.0', 'origin/2.3', 'Flow-2.X/Packages/Framework/TYPO3.Fluid'),
 			array('3.0', 'refs/tags/2.0.0', 'origin/3.0', 'Flow-3.X/Packages/Framework/TYPO3.Fluid'),
@@ -83,37 +62,26 @@ $projectsToCheck = array(
 	'TYPO3.Eel' => array(
 		'gitWebUrl' => 'https://github.com/neos/eel',
 		'releases' => array(
-			array('2.1', 'refs/tags/2.1.0', 'origin/2.1', 'Flow-2.X/Packages/Framework/TYPO3.Eel'),
 			array('2.2', 'refs/tags/2.1.0', 'origin/2.2', 'Flow-2.X/Packages/Framework/TYPO3.Eel'),
 			array('2.3', 'refs/tags/2.1.0', 'origin/2.3', 'Flow-2.X/Packages/Framework/TYPO3.Eel'),
 			array('3.0', 'refs/tags/2.1.0', 'origin/3.0', 'Flow-3.X/Packages/Framework/TYPO3.Eel'),
 			array('master', 'refs/tags/2.1.0', 'origin/master', 'Flow-master/Packages/Framework/TYPO3.Eel'),
 		),
 		'ignoreList' => array(
-			'2.1' => array(
-				'Ib39d7b96f4c5833a304cfc12c5dc7b3c5f53ec21' => 'Replaced by Ieaa3d21afe4c543b83ec381a094515186dda3c2b',
-			),
 		),
 	),
 	'TYPO3.Party' => array(
 		'gitWebUrl' => 'https://github.com/neos/party',
 		'releases' => array(
-			array('2.0', 'refs/tags/FLOW3-1.0.0', 'origin/2.0', 'Flow-2.X/Packages/Framework/TYPO3.Party'),
-			array('2.1', 'refs/tags/2.0.0', 'origin/2.1', 'Flow-2.X/Packages/Framework/TYPO3.Party'),
 			array('2.2', 'refs/tags/2.0.0', 'origin/2.2', 'Flow-2.X/Packages/Framework/TYPO3.Party'),
 			array('2.3', 'refs/tags/2.0.0', 'origin/2.3', 'Flow-2.X/Packages/Framework/TYPO3.Party'),
 		),
 		'ignoreList' => array(
-			'2.0' => array(
-				'47881' => 'Not for 2.0, as the affected validator is not in 2.0'
-			),
 		),
 	),
 	'TYPO3.Kickstart' => array(
 		'gitWebUrl' => 'https://github.com/neos/kickstart',
 		'releases' => array(
-			array('2.0', 'refs/tags/FLOW3-1.0.0', 'origin/2.0', 'Flow-2.X/Packages/Framework/TYPO3.Kickstart'),
-			array('2.1', 'refs/tags/2.0.0', 'origin/2.1', 'Flow-2.X/Packages/Framework/TYPO3.Kickstart'),
 			array('2.2', 'refs/tags/2.0.0', 'origin/2.2', 'Flow-2.X/Packages/Framework/TYPO3.Kickstart'),
 			array('2.3', 'refs/tags/2.0.0', 'origin/2.3', 'Flow-2.X/Packages/Framework/TYPO3.Kickstart'),
 			array('3.0', 'refs/tags/2.0.0', 'origin/3.0', 'Flow-3.X/Packages/Framework/TYPO3.Kickstart'),
@@ -123,8 +91,6 @@ $projectsToCheck = array(
 	'TYPO3.Welcome' => array(
 		'gitWebUrl' => 'https://github.com/neos/flow-welcome',
 		'releases' => array(
-			array('2.0', 'refs/tags/FLOW3-1.0.0', 'origin/2.0', 'Flow-2.X/Packages/Framework/TYPO3.Welcome'),
-			array('2.1', 'refs/tags/2.0.0', 'origin/2.1', 'Flow-2.X/Packages/Framework/TYPO3.Welcome'),
 			array('2.2', 'refs/tags/2.0.0', 'origin/2.2', 'Flow-2.X/Packages/Framework/TYPO3.Welcome'),
 			array('2.3', 'refs/tags/2.0.0', 'origin/2.3', 'Flow-2.X/Packages/Framework/TYPO3.Welcome'),
 			array('3.0', 'refs/tags/2.0.0', 'origin/3.0', 'Flow-3.X/Packages/Framework/TYPO3.Welcome'),
--- neos-base-dist.php
+++ neos-base-dist.php
@@ -15,7 +15,6 @@ $projectsToCheck = array(
 		'gitWebUrl' => 'https://github.com/neos/neos-base-distribution',
 		'releases' => array(
 				# project, starting point, branch, working copy path
-			array('1.0', 'refs/tags/1.0.0', 'origin/1.0', 'Neos-1.0'),
 			array('1.1', 'refs/tags/1.0.0', 'origin/1.1', 'Neos-1.1'),
 			array('1.2', 'refs/tags/1.0.0', 'origin/1.2', 'Neos-1.2'),
 			array('2.0', 'refs/tags/1.0.0', 'origin/2.0', 'Neos-2.0'),
@@ -25,24 +24,12 @@ $projectsToCheck = array(
 	'TYPO3.Neos' => array(
 		'gitWebUrl' => 'https://github.com/neos/neos',
 		'releases' => array(
-			array('1.0', 'bf492f9fe207a490a068390a2af270cb2332c835', 'origin/1.0', 'Neos-1.0/Packages/Application/TYPO3.Neos'),
 			array('1.1', 'bf492f9fe207a490a068390a2af270cb2332c835', 'origin/1.1', 'Neos-1.1/Packages/Application/TYPO3.Neos'),
 			array('1.2', 'bf492f9fe207a490a068390a2af270cb2332c835', 'origin/1.2', 'Neos-1.2/Packages/Application/TYPO3.Neos'),
 			array('2.0', 'bf492f9fe207a490a068390a2af270cb2332c835', 'origin/2.0', 'Neos-2.0/Packages/Application/TYPO3.Neos'),
 			array('master', 'bf492f9fe207a490a068390a2af270cb2332c835', 'origin/master', 'Neos-master/Packages/Application/TYPO3.Neos'),
 		),
 		'ignoreList' => array(
-			'1.0' => array(
-				'I989e33743da6dd93076dfcac957fa2376bd84054' => 'Wrong releases footer, came from 1.0',
-				'I09ae20ae62087ae6b7d4eeb183a3b45e0b5bafbd' => 'Not fixing for 1.0 anymore',
-				'NEOS-173' => 'Not fixing for 1.0 anymore',
-				'NEOS-361' => 'Not fixing for 1.0 anymore',
-				'NEOS-368' => 'Not fixing for 1.0 anymore',
-				'NEOS-352' => 'Not fixing for 1.0 anymore',
-				'NEOS-585' => 'Not fixing for 1.0 anymore',
-				'Id7baa52bd72cf6e53d4ba8dee9cad8f54979aa40' => 'Not fixing for 1.0 anymore',
-				'I78ee51099beef563c56c242bc3b2e6ce10efd4f7' => 'Not fixing for 1.0 anymore'
-			),
 			'1.1' => array(
 				'Iee3fc9449616e86f9f5506d79937b7c7a31103cf' => 'Not needed anymore'
 			),
@@ -58,22 +45,17 @@ $projectsToCheck = array(
 	'TYPO3.Neos.NodeTypes' => array(
 		'gitWebUrl' => 'https://github.com/neos/neos-nodetypes',
 		'releases' => array(
-			array('1.0', 'b3c147d2c17309fde9e36a09a72f70782248f9e2', 'origin/1.0', 'Neos-1.0/Packages/Application/TYPO3.Neos.NodeTypes'),
 			array('1.1', 'b3c147d2c17309fde9e36a09a72f70782248f9e2', 'origin/1.1', 'Neos-1.1/Packages/Application/TYPO3.Neos.NodeTypes'),
 			array('1.2', 'b3c147d2c17309fde9e36a09a72f70782248f9e2', 'origin/1.2', 'Neos-1.2/Packages/Application/TYPO3.Neos.NodeTypes'),
 			array('2.0', 'b3c147d2c17309fde9e36a09a72f70782248f9e2', 'origin/2.0', 'Neos-2.0/Packages/Application/TYPO3.Neos.NodeTypes'),
 			array('master', 'b3c147d2c17309fde9e36a09a72f70782248f9e2', 'origin/master', 'Neos-master/Packages/Application/TYPO3.Neos.NodeTypes'),
 		),
 		'ignoreList' => array(
-			'1.0' => array(
-				'Ie9c134214203d3a928da601e37a4698a9f4ba5a3' => 'Not fixing for 1.0 anymore'
-			),
 		),
 	),
 	'TYPO3.SiteKickstarter' => array(
 		'gitWebUrl' => 'https://github.com/neos/neos-kickstarter',
 		'releases' => array(
-			array('1.0', 'ac71f441e74dc3c5ee8a17480a5c3c96ba21b684', 'origin/1.0', 'Neos-1.0/Packages/Application/TYPO3.SiteKickstarter'),
 			array('1.1', 'ac71f441e74dc3c5ee8a17480a5c3c96ba21b684', 'origin/1.1', 'Neos-1.1/Packages/Application/TYPO3.SiteKickstarter'),
 			array('1.2', 'ac71f441e74dc3c5ee8a17480a5c3c96ba21b684', 'origin/1.2', 'Neos-1.2/Packages/Application/TYPO3.SiteKickstarter'),
 			array('2.0', 'ac71f441e74dc3c5ee8a17480a5c3c96ba21b684', 'origin/2.0', 'Neos-2.0/Packages/Application/TYPO3.SiteKickstarter'),
@@ -83,42 +65,28 @@ $projectsToCheck = array(
 	'TYPO3.TYPO3CR' => array(
 		'gitWebUrl' => 'https://github.com/neos/typo3cr',
 		'releases' => array(
-			array('1.0', '444707e6176e23e67ce40f7d396b0d2229799411', 'origin/1.0', 'Neos-1.0/Packages/Application/TYPO3.TYPO3CR'),
 			array('1.1', '444707e6176e23e67ce40f7d396b0d2229799411', 'origin/1.1', 'Neos-1.1/Packages/Application/TYPO3.TYPO3CR'),
 			array('1.2', '444707e6176e23e67ce40f7d396b0d2229799411', 'origin/1.2', 'Neos-1.2/Packages/Application/TYPO3.TYPO3CR'),
 			array('2.0', '444707e6176e23e67ce40f7d396b0d2229799411', 'origin/2.0', 'Neos-2.0/Packages/Application/TYPO3.TYPO3CR'),
 			array('master', '444707e6176e23e67ce40f7d396b0d2229799411', 'origin/master', 'Neos-master/Packages/Application/TYPO3.TYPO3CR'),
 		),
 		'ignoreList' => array(
-			'1.0' => array(
-				'I99acc1142eeaac82f443f5efa1512feb7766ed1f' => 'Wrong releases footer, does not apply to 1.0',
-				'NEOS-879' => 'Not fixing for 1.0 anymore',
-				'I3dc57f55ba052bee2399ba5b97e5f985fd0a4e3a' => 'Not fixing for 1.0 anymore',
-				'If4d3dff4027f1711a548b75ea78ec45682b89773' => 'Not fixing for 1.0 anymore',
-				'NEOS-170' => 'Not fixing for 1.0 anymore',
-				'I56f8e922f84c00d9402837591d308583f7069b3f' => 'Not fixing for 1.0 anymore'
-			),
 		),
 	),
 	'TYPO3.TypoScript' => array(
 		'gitWebUrl' => 'https://github.com/neos/typoscript',
 		'releases' => array(
-			array('1.0', '681aa987a62d555b77d8398f38cc6280cb0c5fd6', 'origin/1.0', 'Neos-1.0/Packages/Application/TYPO3.TypoScript'),
 			array('1.1', '681aa987a62d555b77d8398f38cc6280cb0c5fd6', 'origin/1.1', 'Neos-1.1/Packages/Application/TYPO3.TypoScript'),
 			array('1.2', '681aa987a62d555b77d8398f38cc6280cb0c5fd6', 'origin/1.2', 'Neos-1.2/Packages/Application/TYPO3.TypoScript'),
 			array('2.0', '681aa987a62d555b77d8398f38cc6280cb0c5fd6', 'origin/2.0', 'Neos-2.0/Packages/Application/TYPO3.TypoScript'),
 			array('master', '681aa987a62d555b77d8398f38cc6280cb0c5fd6', 'origin/master', 'Neos-master/Packages/Application/TYPO3.TypoScript'),
 		),
 		'ignoreList' => array(
-			'1.0' => array(
-				'Idffd41450c3c210508795aaeda69ac4e199cc54e' => 'Not fixing for 1.0 anymore'
-			),
 		),
 	),
 	'TYPO3.NeosDemoTypo3Org' => array(
 		'gitWebUrl' => 'https://github.com/neos/ndoesdemotypo3org',
 		'releases' => array(
-			array('1.0', '4766bdd615b63bcefc8d2862738e33a2b3313448', 'origin/1.0', 'Neos-1.0/Packages/Sites/TYPO3.NeosDemoTypo3Org'),
 			array('1.1', '4766bdd615b63bcefc8d2862738e33a2b3313448', 'origin/1.1', 'Neos-1.1/Packages/Sites/TYPO3.NeosDemoTypo3Org'),
 			array('1.2', '4766bdd615b63bcefc8d2862738e33a2b3313448', 'origin/1.2', 'Neos-1.2/Packages/Sites/TYPO3.NeosDemoTypo3Org'),
 			array('2.0', '4766bdd615b63bcefc8d2862738e33a2b3313448', 'origin/2.0', 'Neos-2.0/Packages/Sites/TYPO3.NeosDemoTypo3Org'),
@@ -128,17 +96,12 @@ $projectsToCheck = array(
 	'TYPO3.Media' => array(
 		'gitWebUrl' => 'https://github.com/neos/media',
 		'releases' => array(
-			array('1.0', '70290a04f07329ccb725181c6d7e028e0aeac483', 'origin/1.0', 'Neos-1.0/Packages/Application/TYPO3.Media'),
 			array('1.1', '3189fc5ec371b46c012443b0c5cb49c225c76b1e', 'origin/1.1', 'Neos-1.1/Packages/Application/TYPO3.Media'),
 			array('1.2', '3189fc5ec371b46c012443b0c5cb49c225c76b1e', 'origin/1.2', 'Neos-1.2/Packages/Application/TYPO3.Media'),
 			array('2.0', '3189fc5ec371b46c012443b0c5cb49c225c76b1e', 'origin/2.0', 'Neos-2.0/Packages/Application/TYPO3.Media'),
 			array('master', '70290a04f07329ccb725181c6d7e028e0aeac483', 'origin/master', 'Neos-master/Packages/Application/TYPO3.Media'),
 		),
 		'ignoreList' => array(
-			'1.0' => array(
-				'NEOS-414' => 'Not fixing for 1.0 anymore',
-				'Icbf9110b19bdd705a8db7e2d67521225651b6dad' => 'Not fixing for 1.0 anymore'
-			),
 		),
 	)
 );
